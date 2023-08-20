7a8a5dd0bdc22bc58ca9a5c6ba755e0884d8780c
Ahmed Salama <ahmed.salama@ndgit.com>
2023-08-20 17:23:28 +0200

    add controller test cases

--- src/test/java/com/allane/leasing/controller/LeasingContractControllerTest.java
+++ src/test/java/com/allane/leasing/controller/LeasingContractControllerTest.java
@@ -7,26 +7,29 @@ import com.allane.leasing.dto.leasingcontract.ContractOverviewDetailsResponseDto
 import com.allane.leasing.dto.leasingcontract.ContractOverviewResponseDto;
 import com.allane.leasing.dto.leasingcontract.LeasingContractDetailsResponseDto;
 import com.allane.leasing.dto.leasingcontract.LeasingContractRequestDto;
+import com.allane.leasing.dto.leasingcontract.LeasingContractUpdateRequestDto;
 import com.allane.leasing.exception.NotFoundException;
-import com.allane.leasing.service.CustomerService;
 import com.allane.leasing.service.LeasingContractService;
-import com.allane.leasing.service.VehicleService;
 import com.allane.leasing.stub.leasingcontract.ContractOverviewDetailsResponseDtoStub;
 import com.allane.leasing.stub.leasingcontract.ContractOverviewResponseDtoStub;
 import com.allane.leasing.stub.leasingcontract.LeasingContractDetailsResponseDtoStub;
 import com.allane.leasing.stub.leasingcontract.LeasingContractRequestDtoStub;
+import com.allane.leasing.stub.leasingcontract.LeasingContractUpdateRequestDtoStub;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,12 +41,6 @@ class LeasingContractControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @MockBean
-    private CustomerService customerService;
-
-    @MockBean
-    private VehicleService vehicleService;
 
     @MockBean
     private LeasingContractService leasingContractService;
@@ -174,22 +171,199 @@ class LeasingContractControllerTest {
     }
 
     @Test
-    void shouldAssignVehicle() {
+    void shouldAssignVehicle() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String vehicleId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doNothing().when(leasingContractService)
+                .assignVehicle(UUID.fromString(leasingContractId), UUID.fromString(vehicleId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/vehicle/" + vehicleId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isOk());
     }
 
     @Test
-    void shouldUnAssignVehicle() {
+    void shouldThrowNotFoundWhenNoContractFoundAssignVehicle() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String vehicleId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Contract not found"))
+                .when(leasingContractService)
+                .assignVehicle(UUID.fromString(leasingContractId), UUID.fromString(vehicleId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/vehicle/" + vehicleId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Contract not found"));
     }
 
     @Test
-    void shouldAssignCustomer() {
+    void shouldThrowNotFoundWhenNoVehicleFoundAssign() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String vehicleId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Vehicle not found"))
+                .when(leasingContractService)
+                .assignVehicle(UUID.fromString(leasingContractId), UUID.fromString(vehicleId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/vehicle/" + vehicleId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Vehicle not found"));
     }
 
     @Test
-    void shouldUnAssignCustomer() {
+    void shouldUnAssignVehicle() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String vehicleId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doNothing().when(leasingContractService).unAssignVehicle(UUID.fromString(leasingContractId),
+                                                                 UUID.fromString(vehicleId));
+
+        mvc.perform(post("/contracts/unassign/" + leasingContractId + "/vehicle/" + vehicleId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isOk());
     }
 
     @Test
-    void shouldUpdateLeasingContract() {
+    void shouldAssignCustomer() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doNothing().when(leasingContractService)
+                .assignCustomer(UUID.fromString(leasingContractId), UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldThrowNotFoundWhenNoContractFound() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Contract not found"))
+                .when(leasingContractService)
+                .assignCustomer(UUID.fromString(leasingContractId), UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Contract not found"));
+    }
+
+    @Test
+    void shouldThrowNotFoundWhenNoCustomer() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Customer not found"))
+                .when(leasingContractService)
+                .assignCustomer(UUID.fromString(leasingContractId), UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/assign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Customer not found"));
+    }
+
+    @Test
+    void shouldUnAssignCustomer() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doNothing()
+                .when(leasingContractService)
+                .unAssignVehicle(UUID.fromString(leasingContractId),
+                                 UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/unassign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldThrowNotFoundCustomerWhenUnAssignCustomer() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Customer not found"))
+                .when(leasingContractService)
+                .unAssignCustomer(UUID.fromString(leasingContractId), UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/unassign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Customer not found"));
+    }
+
+    @Test
+    void shouldThrowNotFoundContractWhenUnAssignCustomer() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+        String customerId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        doThrow(new NotFoundException("Contract not found"))
+                .when(leasingContractService)
+                .unAssignCustomer(UUID.fromString(leasingContractId), UUID.fromString(customerId));
+
+        mvc.perform(post("/contracts/unassign/" + leasingContractId + "/customer/" + customerId)
+                            .accept(APPLICATION_JSON)
+                            .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.zonedDateTime").exists())
+                .andExpect(jsonPath("$.message").value("Contract not found"));
+    }
+
+    @Test
+    void shouldUpdateLeasingContract() throws Exception {
+        String leasingContractId = "eab78474-3329-42a1-b8b8-b13efd3c5572";
+
+        LeasingContractUpdateRequestDto dto = LeasingContractUpdateRequestDtoStub.getDto();
+        LeasingContractDetailsResponseDto responseDto = LeasingContractDetailsResponseDtoStub.getDto();
+        responseDto.setContractNumber(152);
+
+        given(leasingContractService.updateLeasingContract(UUID.fromString(leasingContractId), dto))
+                .willReturn(responseDto);
+
+        mvc.perform(put("/contracts/" + leasingContractId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(dto)))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.contractNumber").value(152))
+                .andExpect(jsonPath("$.monthlyRate").value(dto.getMonthlyRate().toString()));
     }
 }
